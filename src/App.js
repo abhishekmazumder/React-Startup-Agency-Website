aec63f6bf56e0a7dd39183e390e766d25a4e532f
abhishek <abhishekmazumder69@gmail.com>
2020-05-18 19:30:58 +0530

    dyanamic header and about pages created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// pages
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Route, link } from 'react-router-dom';
+import About from './components/pages/About'
 
 function App() {
 	return (
 		<Router>
 			<PageWrapper>
-				<Route path="/" component={Home} />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route  exact path="/about" component={About} />
+				</Switch>
 			</PageWrapper>
 		</Router>
 	);
 }
+
 
 export default App;
